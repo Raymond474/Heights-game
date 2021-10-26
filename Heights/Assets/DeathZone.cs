dbfb646c4c782bf8b2cdd2654237593d61a68a35
Raymond Martinez <Raymond474@users.noreply.github.com>
2021-10-26 14:13:53 -0700

    Done

--- Heights/Assets/DeathZone.cs
+++ Heights/Assets/DeathZone.cs
@@ -11,7 +11,7 @@ public class DeathZone : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        //gameOver.Setup(collision);
+        gameOver.Setup(collision);
         /*
         if (!easyMode)
         {
